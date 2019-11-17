8b8831d265da1b886f7fda767498b76c11130b6a
Cole Tramel <cole.b.tramel@gmail.com>
2019-11-17 13:47:21 -0600

    Added sliders, buttons, and color gradient for mass

--- canvas.js
+++ canvas.js
@@ -1,17 +1,96 @@
 function updateMass1(newMass1) {
-  //todo
+  var display = document.getElementById("initialMass1Value");
+  display.innerHTML = newMass1;
+  m1 = Number(newMass1);
+  reset();
 }
 
 function updateMass2(newMass2) {
-  //todo
+  var display = document.getElementById("initialMass2Value");
+  display.innerHTML = newMass2;
+  m2 = Number(newMass2);
+  reset();
 }
 
 function updateGravity(newGrav) {
-  //todo
+  var display = document.getElementById("gravityVal");
+  display.innerHTML = newGrav;
+  g = Number(newGrav);
+  reset();
 }
 
 function updateMu(newMu) {
-  //todo
+  var display = document.getElementById("muVal");
+  display.innerHTML = newMu;
+  mu = Number(newMu);
+  reset();
+}
+
+function reset() {
+  heightM2 = innerHeight / 3.19; // height of second box
+  distM1 = innerWidth / 3.84;  // horizontal distance of first box
+  v = 0; // reset velocity
+  w1 = m1 * g;
+  w2 = m2 * g;
+  a = scale * ((w2 - mu * w1) / (m1 + m2));
+  drawTableAndPulley();
+}
+
+function drawTableAndPulley() {
+  /* all the decimals are to get
+  the ratios right for different
+  screen sizes - may be adjusted later */
+
+  c.clearRect(0, 0, innerWidth, innerHeight / 1.5);
+  // table
+  c.beginPath();
+  c.moveTo(0, innerHeight / 4.31);
+  c.lineTo(innerWidth / 2.05, innerHeight / 4.31);
+  c.lineTo(innerWidth / 2.05, innerHeight / 1.43);
+  c.stroke();
+
+  // pulley
+  c.moveTo(innerWidth / 2.05, innerHeight / 4.31);
+  c.lineTo(innerWidth / 2.0, innerHeight / 4.78);
+  c.stroke();
+  c.beginPath();
+  c.arc(innerWidth / 1.94, innerHeight / 5.56, innerHeight / 28.73, 0, Math.PI * 2, false);
+  c.stroke();
+
+  // vertical line and box
+  c.moveTo(innerWidth / 1.89, innerHeight / 4.92);
+  c.lineTo(innerWidth / 1.89, heightM2);
+  c.stroke();
+  c.strokeRect(innerWidth / 1.97, heightM2, innerHeight / 14.36, innerHeight / 14.36);
+  var hex2 = parseInt((((m2 / 15.0) * 130) + 125).toString());
+  var hexM2 = hex2.toString(16); // color based on mass
+  c.fillStyle = "#" + hexM2 + "0000";
+  c.fillRect(innerWidth / 1.97, heightM2, innerHeight / 14.36, innerHeight / 14.36);
+
+  // horizontal line and box
+  c.moveTo(innerWidth / 1.94 - innerHeight / 28.73, innerHeight / 5.56);
+  c.lineTo(distM1, innerHeight / 5.56);
+  c.stroke();
+  var hex1 = parseInt((((m1 / 15.0) * 130) + 125).toString());
+  var hexM1 = hex1.toString(16); // color based on mass
+  c.fillStyle = "#0000" + hexM1;
+  c.strokeRect(distM1 - innerHeight / 11.49, innerHeight / 6.89, innerHeight / 11.49, innerHeight / 11.49);
+  c.fillRect(distM1 - innerHeight / 11.49, innerHeight / 6.89, innerHeight / 11.49, innerHeight / 11.49);
+
+  // acceleration text
+  c.fillStyle = "black";
+  var aRounded = Math.round((a / scale) * 100) / 100;
+  var accelText = aRounded.toString();
+  if (a < 0) {
+    accelText = "0";
+  }
+  c.font = "24pt Arial";
+  c.fillText("Acceleration: " + accelText, innerWidth / 1.7, innerHeight / 8.62);
+
+  // velocity text
+  var vRounded = Math.round(v * 100) / 100;
+  var vText = vRounded.toString();
+  c.fillText("Velocity: " + vText, innerWidth / 1.7, innerHeight / 5.5);
 }
 
 var canvas = document.querySelector("canvas");
@@ -35,45 +114,16 @@ var distM1 = innerWidth / 3.84;  // horizontal distance of first box
 
 var v = 0;
 
+drawTableAndPulley();
+
 function animate() {
-  /* all the decimals are to get
-  the ratios right for different
-  screen sizes - may be adjusted later */
-
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight / 1.5);
-  // table
-  c.beginPath();
-  c.moveTo(0, innerHeight / 4.31);
-  c.lineTo(innerWidth / 2.05, innerHeight / 4.31);
-  c.lineTo(innerWidth / 2.05, innerHeight / 1.43);
-  c.stroke();
-
-  // pulley
-  c.moveTo(innerWidth / 2.05, innerHeight / 4.31);
-  c.lineTo(innerWidth / 2.0, innerHeight / 4.78);
-  c.stroke();
-  c.beginPath();
-  c.arc(innerWidth / 1.94, innerHeight / 5.56, innerHeight / 28.73, 0, Math.PI * 2, false);
-  c.stroke();
-
-  // vertical line and box
-  c.moveTo(innerWidth / 1.89, innerHeight / 4.92);
-  c.lineTo(innerWidth / 1.89, heightM2);
-  c.stroke();
-  c.strokeRect(innerWidth / 1.97, heightM2, innerHeight / 14.36, innerHeight / 14.36);
-
-  // horizontal line and box
-  c.moveTo(innerWidth / 1.94 - innerHeight / 28.73, innerHeight / 5.56);
-  c.lineTo(distM1, innerHeight / 5.56);
-  c.stroke();
-  c.strokeRect(distM1 - innerHeight / 11.49, innerHeight / 6.89, innerHeight / 11.49, innerHeight / 11.49);
+  drawTableAndPulley();
 
   if (a > 0 && heightM2 + innerHeight / 14.36 <= innerHeight / 1.5) { // if no acceleration or box 2 hits the ground
     v += a;
     heightM2 += v; // update positions
     distM1 += v;
+    requestAnimationFrame(animate);
   }
-}
 
-animate();
+}
